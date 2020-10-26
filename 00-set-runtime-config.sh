63f191bbfc895f053f40e490bc41089c6856a097
Dylan Turnbull <dylanturn@gmail.com>
2020-10-26 13:06:56 -0500

    think I finally licked the issue with local storage

--- 00-set-runtime-config.sh
+++ 00-set-runtime-config.sh
@@ -3,6 +3,6 @@ CONFIG_JS_PATH="/usr/share/nginx/html/config.js"
 echo "var config = new Map();" > $CONFIG_JS_PATH
 for value in $(env | grep -ir VUE_APP_)
 do
-  echo "config["\"$(echo $value | cut -d '=' -f 1)\""]=\"$(echo $value | cut -d '=' -f 2-)\";" >> $CONFIG_JS_PATH
+  echo "localStorage.setItem("\"$(echo $value | cut -d '=' -f 1)\"", \"$(echo $value | cut -d '=' -f 2-)\");" >> $CONFIG_JS_PATH
 done
 exit 0
--- public/config.js
+++ public/config.js
@@ -1,4 +1,4 @@
 // The default development configs can go here.
 // These congs will be overwritten by the entrypoint file with environment specific values.
-var config = {};
-config["VUE_APP_API_ENDPOINT"] = "https://localhost:5001";
+
+localStorage.setItem("VUE_APP_API_ENDPOINT", "https://localhost:5001");
