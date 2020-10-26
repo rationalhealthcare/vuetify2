6df6b15e70084950a5bd4f1b83fd88ec2880de85
Dylan Turnbull <dylanturn@gmail.com>
2020-10-26 12:39:49 -0500

    update the config

--- 00-set-runtime-config.sh
+++ 00-set-runtime-config.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 CONFIG_JS_PATH="/usr/share/nginx/html/config.js"
-echo "let config = new Map();" > $CONFIG_JS_PATH
+echo "var config = new Map();" > $CONFIG_JS_PATH
 for value in $(env | grep -ir VUE_APP_)
 do
   echo "config["\"$(echo $value | cut -d '=' -f 1)\""]=\"$(echo $value | cut -d '=' -f 2-)\";" >> $CONFIG_JS_PATH
--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // This is required for the build to work.
 let dummyconfig = {};
-var config = (config === undefined) ? dummyconfig : config;
+let config = (config === undefined) ? dummyconfig : config;
